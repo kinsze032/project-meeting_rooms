5373b51c8cb2c5c94d5fc94168d5ea8016f13ddf
kinsze032 <szedzielarzkinga@gmail.com>
2023-02-09 00:17:35 +0100

    Stworzenie pierwszego widoku

---
+++ reservation/reservation_app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class ConferenceRoom(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    capacity = models.PositiveIntegerField()
+    projector_availability = models.BooleanField(default=False)
