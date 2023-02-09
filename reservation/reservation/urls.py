6784da199787d4cecc546e1384c088382cd58235
kinsze032 <szedzielarzkinga@gmail.com>
2023-02-09 23:02:58 +0100

    Lista sal

--- reservation/reservation/urls.py
+++ reservation/reservation/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from reservation_app.views import HomeView, AddRoomView
+from reservation_app.views import HomeView, AddRoomView, RoomListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('home/', HomeView.as_view(), name='home'),
     path('room/new/', AddRoomView.as_view(), name='add-room'),
+    path('room/list/', RoomListView.as_view(), name="rooms-list"),
 ]
--- reservation/reservation_app/views.py
+++ reservation/reservation_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from reservation_app.models import ConferenceRoom
 
 
 # Create your views here.
@@ -11,3 +12,32 @@ class HomeView(View):
 class AddRoomView(View):
     def get(self, request):
         return render(request, "add_room.html")
+
+    def post(self, request):
+        name = request.POST.get('room-name')
+        capacity = request.POST.get('capacity')
+        if capacity:
+            capacity = int(capacity)
+        else:
+            capacity = 0
+        projector = request.POST.get('projector')
+        if projector == 'true':
+            projector = True
+        else:
+            projector = False
+
+        if not name:
+            return render(request, 'add_room.html', context={'form_error': 'Nie podano nawy sali'})
+        if capacity <= 0:
+            return render(request, 'add_room.html', context={'form_error': 'Pojemność sali musi być dodatnia'})
+        if ConferenceRoom.objects.filter(name=name).first():
+            return render(request, 'add_room.html', context={'form_error': 'Sala o podanej nazwie już istnieje'})
+
+        ConferenceRoom.objects.create(name=name, capacity=capacity, projector_availability=projector)
+        return redirect('rooms-list')
+
+
+class RoomListView(View):
+    def get(self, request):
+        rooms = ConferenceRoom.objects.all()
+        return render(request, "rooms.html", context={"rooms": rooms})
